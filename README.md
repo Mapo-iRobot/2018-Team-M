8f20efdaf6f7ec26a226692094fe61f5bc108f9f
Sangwan Jeon <maxswjeon@naver.com>
2018-10-23 21:07:22 +0900

    Added and Edited File names by Teams
    팀 이름별로 파일 나누기
    README 파일을 수정

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# 2018-Team-M
+# FLL 2018
+2018년도 FLL(First Lego Leauge)의 소프트웨어에 대해 논의와 버전관리가 이루어질 Github Repository입니다.  
+시간이 여유롭지 않은 터라 같은 Repository에서 파일만 다르게 운영하는 것으로 하였습니다.
+
+## Rules
+1. 한 Commit당 하나의 파일만 수정합니다.
+2. 몰아서 커밋하지 않습니다. 나눠서 Commit하는 일이 있더라도 즉각적으로 Commit합니다.
+3. 항상 실행가능한 상태로 Commit하는 것을 목표로 합니다.
---
+++ Team M.c
@@ -0,0 +1,42 @@
+#define wheelL motorA
+#define wheelR motorD
+
+#define motor1 motorB
+#define motor2 motorC
+
+#define SYSTEM_ALL ~0
+#define SYSTEM_WHEEL			0x00000001
+#define STSTEM_MOTOR			0x00000002
+#define SYSTEM_ENCODER_WHEEL	0x00000004
+#define SYSTEM_ENCODER_MOTOR	0x00000008
+#define SYSTEM_GYROSEN			0x00000010
+
+
+void stopMotor() {
+	for (int i = motor[wheelL]; i >= 0; ++i) {
+
+	}
+}
+
+void Init(int flag = SYSTEM_ALL) {
+	if (flag & SYSTEM_WHEEL) {
+		motor[wheelL] = 0;
+		motor[wheelR] = 0;
+	}
+	
+	if (flag & SYSTEM_MOTOR) {
+		motor[motor1] = 0;
+		motor[motor2] = 0;
+	}
+	
+	if (flag & SYSTEM_ENCODER_WHEEL) {
+		
+	}
+}
+
+task main()
+{
+
+
+
+}
